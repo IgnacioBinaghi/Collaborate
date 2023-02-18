24d694461fef86f0f9cd15b040a4b0678483270e
isaac1000000 <isaac.fisher@nyu.edu>
2023-02-18 16:30:47 -0500

    push.py updates

--- io/push.py
+++ io/push.py
@@ -1,9 +1,32 @@
-# Controls all push functions to the database
+""" 
+Controls all push functions to the database
+Isaac
+"""
 from pymongo import MongoClient
 from basic import get_collection
 import certifi
+import time
 
+collection = get_collection()
 
+def current_user():
+	# Returns id of the current user from resources/id.txt
+	with open("../resources/id.txt", "r") as r:
+		return r.readline()
+
+def push_to_db(user):
+	# Pushes the user to the database with a unique id
+	user["_id"] = str(time.time())
+	collection.insert_one(user)
+	with open("../resources/id.txt", "w") as w:
+		w.write(user["_id"])
+	return user
+	
+def push_like_to(user_id):
+	# Pushes the current user to the given user's LikedBy list
+	query = {"_id": user_id}
+	newvalue = {"$push": {"likedby": current_user()}}
+	collection.update_one(query, newvalue)
 
 """
 user_template = {
