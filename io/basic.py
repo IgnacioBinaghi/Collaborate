ad58722e3cbaeb7fb905f2fa7c3581886872e7d9
isaac1000000 <isaac.fisher@nyu.edu>
2023-02-18 15:35:16 -0500

    Create basic.py

---
+++ io/basic.py
@@ -0,0 +1,19 @@
+"""
+This file provides basic operations for getting the database and collection
+Isaac
+""""
+from pymongo import MongoClient
+import certifi
+
+
+def get_database():
+   # Gets the database containing the collection of users
+    ca = certifi.where()
+    client = MongoClient("mongodb+srv://isaacfisher:9yqMyqfX1AlJ2p5x@collaborate.szhuyon.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+    return client['collaborate']
+
+def get_collection():
+   # Gets the collection to push users to
+   dbname = get_database()
+   collection_name = dbname["users"]
+   return collection_name
