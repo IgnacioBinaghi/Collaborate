9619868dbe53605326a97a4daa44fb3532ada043
isaac1000000 <isaac.fisher@nyu.edu>
2023-02-18 15:31:12 -0500

    get_database added

--- io/push.py
+++ io/push.py
@@ -1,1 +1,19 @@
 # Controls all push functions to the database
+from pymongo import MongoClient
+import certifi
+
+
+def get_database():
+
+   ca = certifi.where()
+   client = MongoClient("mongodb+srv://isaacfisher:9yqMyqfX1AlJ2p5x@collaborate.szhuyon.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['collaborate']
+  
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":   
+  
+   # Get the database
+   dbname = get_database()
+   print(dbname)
