7ef01c380e138fc71379b2e8361d772025f5a156
isaac1000000 <isaac.fisher@nyu.edu>
2023-02-18 18:17:15 -0500

    Update pull.py

--- ppio/pull.py
+++ ppio/pull.py
@@ -1,1 +1,13 @@
 # Controlls all pull functions in the database
+from .basic import get_collection
+
+def login(username, password):
+	user = get_collection().find_one({"user": username})
+	if (user == None):
+		return False
+	if (user["pass"] != password):	
+		return False
+	else:
+		with open("resources/id.txt", "w") as w:
+			w.write(user["_id"])
+			return True
