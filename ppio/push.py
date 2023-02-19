47d5928afa2c4b5efc4d4fc1f6951645445b0736
IgnacioBinaghi <binaghiignacio@gmail.com>
2023-02-19 01:11:27 -0500

    allchanges

--- ppio/push.py
+++ ppio/push.py
@@ -9,7 +9,7 @@ import time
 
 def current_user():
 	# Returns id of the current user from resources/id.txt
-	with open("../resources/id.txt", "r") as r:
+	with open("resources/id.txt", "r") as r:
 		return r.readline()
 
 def push_to_db(user):
@@ -27,4 +27,45 @@ def push_like_to(user_id):
 	# Pushes the current user to the given user's LikedBy list
 	query = {"_id": user_id}
 	newvalue = {"$push": {"likedby": current_user()}}
+	get_collection().update_one(query, newvalue)
+
+def push_match_to(user_id):
+	# Pushes the current user to the current and given user's matches list
+	curr_user = current_user()
+	collection = get_collection()
+	query = {"_id": user_id}
+	newvalue = {"$push": {"matches": curr_user}}
+	collection.update_one(query, newvalue)
+	query = {"_id": curr_user}
+	newvalue = {"$push": {"matches": user_id}}
+	collection.update_one(query, newvalue)
+
+def update_courses(courses):
+	# Pushes the courses to the currents users course string
+	query = {"_id": current_user()}
+	newvalue = {"$set": {"courses": courses}}
+	get_collection().update_one(query, newvalue)
+
+def update_topics(topics):
+	# Pushes the topics to the current user's topics string
+	query = {"_id": current_user()}
+	newvalue = {"$set": {"topics": topics}}
+	get_collection().update_one(query, newvalue)
+
+def update_area(area):
+	# Pushes the area to the given user's area string
+	query = {"_id": current_user()}
+	newvalue = {"$set": {"Area": area}}
+	get_collection().update_one(query, newvalue)
+
+def update_bio(bio):
+	# Pushes the bio to the given user's bio string
+	query = {"_id": current_user()}
+	newvalue = {"$set": {"bio": bio}}
+	get_collection().update_one(query, newvalue)
+
+def update_insta(insta):
+	# Pushes the current user to the given user's LikedBy list
+	query = {"_id": current_user()}
+	newvalue = {"$set": {"socials": insta}}
 	get_collection().update_one(query, newvalue)
--- ppio/random_pull.py
+++ ppio/random_pull.py
@@ -1,26 +1,34 @@
 # A generator that will return the next random user
 from .basic import get_collection
 from random import shuffle
+import json
 
-class random_users(object):
 
-	random_user_list = []
 
-	def __init__(self):
-		self.random_user_list = get_collection().distinct("_id")
-		shuffle(self.random_user_list)
+def refresh_rand_list():
+	random_user_list = get_collection().distinct("_id")
+	shuffle(random_user_list)
 
-	def __iter__(self):
-		return self
+	data = {
+		"index" : len(random_user_list) - 1,
+		"IDList" : random_user_list
+	}
 
-	def __next__(self):
-		return self.next()
+	with open("resources/rand.json", "w") as w:
+		json.dump(data, w, indent=4)
 
-	def next(self):
-		if (len(self.random_user_list) == 0):
-			self.random_user_list = get_collection().distinct("_id")
-			shuffle(self.random_user_list)
 
-		res = self.random_user_list[0]
-		self.random_user_list = self.random_user_list[1:]
-		return res
+def next_rand_user():
+	with open("resources/rand.json", "r") as r:
+		data = json.load(r)
+		if data["index"] == 0:
+			refresh_rand_list()
+			return next_rand_user()
+		result = data["IDList"][data["index"]]
+
+	data["index"] -= 1
+
+	with open("resources/rand.json", "w") as w:
+		json.dump(data, w, indent=4)
+
+	return result
